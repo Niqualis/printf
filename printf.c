9850ed2e1e71644fce1b480f7e1ea60e32a32e65
IsaacGreenMachine <zuderg@gmail.c>
2021-08-01 13:58:26 -0700

    added testMain script to test print f, built skeleton for printf and added char handling, added compiled file printfTest that works

--- printf.c
+++ printf.c
@@ -1,10 +1,69 @@
+#include <stdarg.h>
+#include "holberton.h"
 /**
  * _printf - prints passed input
  * @format: how to print passed input
  *
  * Return: the number of characters printed
  */
+
+/*TODO:
+ * use case statement instead of if/else if
+ * implement handling for chars
+ * implement handling for strings
+ * implement handling for percents | DONE
+ * implement handling for ints
+ * implement handling for doubles
+ * implement handling for unknown (%j or %q or something)
+ * fringe cases
+ * 	what if just "%"
+ * 	what if format is blank
+ * 	what if garbage value from ...
+ * 	mismatch between format and passed value (%c and 3 or %i and "hey")
+ *
+ * To run with testMain: gcc testMain.c printf.c _putchar.c -o printfTest 
+ */
 int _printf(const char *format, ...)
 {
+int pos; /*position in format string*/
+va_list ap; /*enables taking input from the ...*/
+va_start(ap, format); 
+	/*loops through format and prints each char*/
+	for (pos = 0; format[pos] != '\0'; pos++)
+	{
+		/*checks for % in format string*/
+		if (format[pos] == '%')
+		{
+		pos++;
+			/*checks if value after % is c (it's a char) */
+			if (format[pos] == 'c')
+			{
+                        _putchar(va_arg(ap, int));
+			}
+			/*checks if value after % is s (it's a string) */
+			else if (format[pos] == 's')
+			{
+                        }
+			/*checks if value after % is % (just print %) */
+			else if (format[pos] == '%')
+			{
+                        _putchar('%');
+			}
+			/*there's a percent at the very end of format string */
+			else if (format[pos] == '\0')
+			{
+			
+			}
+			/*there's a percent sign and not a recognized value after*/
+			else
+			{
+			}
 
+		}
+		else /*if current char in format string is not % */
+		{
+		_putchar(format[pos]); /*print it like normal*/
+		}
+	}
+va_end(ap);
 }
---
+++ testMain.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+int main()
+{
+/*test case: 
+ * prints "testing" 
+ * %s is a false specifier right now so it doesn't print and skips over 
+ * prints 123
+ * prints the char a (because theres a %c for char) 
+ * follows with a new line.
+ *
+ * Feel free to change this test to suit your needs. 
+ * -Isaac
+ * */
+_printf("testing%s123%c\n", 'a');
+}
