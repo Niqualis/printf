2b73432505c28ba04570352cbeaada92257b8cb6
IsaacGreenMachine <zuderg@gmail.c>
2021-08-03 12:58:56 -0700

    appeased betty ona  function I didn't even write you fFOOOOOL

--- _putchar.c
+++ _putchar.c
@@ -2,7 +2,6 @@
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
- * @n: count
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
